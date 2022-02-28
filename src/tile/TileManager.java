9e251a94cfb4ecb174e4c62db7f50c2c7f15bf61
MH210442 <MH210442@reigate.ac.uk>
2022-02-28 10:34:17 +0000

    Lights Fixed

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -78,16 +78,10 @@ public class TileManager {
             }
             else if(gp.keyH.onePressed){
                 if(mapNum[pX][pY]!=1) {
+                    Spiral(5,5,pX,pY,g2,tileNum);
                     mapNum[pX][pY] = 3;
-                    mapNum[pX + 1][pY + 1] = 2;
-                    mapNum[pX + 1][pY - 1] = 2;
-                    mapNum[pX - 1][pY + 1] = 2;
-                    mapNum[pX - 1][pY - 1] = 2;
-                    mapNum[pX - 1][pY] = 2;
-                    mapNum[pX + 1][pY] = 2;
-                    mapNum[pX][pY - 1] = 2;
-                    mapNum[pX][pY + 1] = 2;
-                    g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+                    g2.drawImage(tile[tileNum].image, pX, pY, gp.tileSize, gp.tileSize, null);
+
                 }
 
             }else {
@@ -102,5 +96,29 @@ public class TileManager {
                 y += gp.tileSize;
             }
         }
+    }
+    public void Spiral(int X, int Y,int placeX, int placeY, Graphics2D g3, int tileNum){
+        int x,y,dx,dy;
+        x=y=dx=0;
+        dy=-1;
+        int t = Math.max(X,Y);
+        int maxI = t*t;
+        for (int i = 0; i < maxI; i++) {
+            if((-1*X/2 <= x)&&(x<=X/2)&&(-1*Y/2 <= y)&&(y<= Y/2)){
+
+                if(mapNum[placeX+x][placeY+y] == 0) {
+                    mapNum[placeX+x][placeY+y] = 2;
+                    g3.drawImage(tile[tileNum].image, placeX, placeY, gp.tileSize, gp.tileSize, null);
+                }
+            }
+            if((x==y)||((x<0)&&(x==(-1*y)))||((x>0)&&(x==1-y))){
+                t = dx;
+                dx= -1*dy;
+                dy = t;
+            }
+            x += dx;
+            y += dy;
+        }
+
     }
 }
